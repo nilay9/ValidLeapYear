5be975a42b4114c64950badac88af966d429d4b6
nilay9 <nil.joshi860@gmail.com>
2020-05-18 16:40:19 +0530

    Final commit
    Refactored code

--- src/com/leapyear/LeapYear.java
+++ src/com/leapyear/LeapYear.java
@@ -1,23 +1,31 @@
 package com.leapyear;
 
 public class LeapYear {
-
+	private static final int YEAR_LENGHT = 4;
+	
 	public String checkLeapYear(String year) {
-		int yearLenght = 4;
 		int leapYear;
-		if ( yearLenght != year.length()) { 
-			throw new NumberFormatException();
-		}
-		for (int i = 0; i < yearLenght; i++) {
-			if (!Character.isDigit(year.charAt(i))) {
-				throw new NumberFormatException();
-			} 
-		}
-		leapYear = Integer.parseInt(year);
+		leapYear = validatinInput(year);
+		
+		return findLeapYear(leapYear);
+	}
+
+	private String findLeapYear(int leapYear) {
 		if ((leapYear % 4 == 0) && (leapYear % 100 != 0) || (leapYear % 400 != 0) && (leapYear % 100 == 0)) {
 			return "Leap Year";
 		} 
 		return "Not a Leap Year";
-		
+	}
+
+	private Integer validatinInput(String year) {
+		if ( YEAR_LENGHT != year.length()) { 
+			throw new NumberFormatException();
+		}
+		for (int i = 0; i < YEAR_LENGHT; i++) {
+			if (!Character.isDigit(year.charAt(i))) {
+				throw new NumberFormatException();
+			} 
+		}
+		return Integer.parseInt(year);
 	}
 }
--- test/com/testleapyear/TestLeapYear.java
+++ test/com/testleapyear/TestLeapYear.java
@@ -4,22 +4,29 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.leapyear.LeapYear;
 
 class TestLeapYear {
+	
+	private LeapYear validator;
+	
+	@BeforeEach
+	public void initBefore() {
+		validator = new LeapYear();
+	}
+	
 
 	@Test
 	public void checkA4DigitYear() {
-		LeapYear validator = new LeapYear();
 		String result = validator.checkLeapYear("2008");
 		assertEquals("Leap Year",result);
 	}
 	
 	@Test
 	public void checkMoreThan4DigitYear() {
-		LeapYear validator = new LeapYear();
 		Assertions.assertThrows(NumberFormatException.class, () -> {
 			validator.checkLeapYear("20500");
 		});
@@ -28,21 +35,18 @@ class TestLeapYear {
 	@Test
 	public void checkIfValidIntegerYear() {
 		Assertions.assertThrows(NumberFormatException.class, () -> {
-			LeapYear validator = new LeapYear();
 			validator.checkLeapYear("20a0");
 		});
 	}
 	
 	@Test
 	public void checkIfYearIsDivisibleBy4AndNot100ThanIsALeapYear() {
-			LeapYear validator = new LeapYear();
 			String result = validator.checkLeapYear("2012");
 			assertEquals("Leap Year",result);
 	}
 	
 	@Test
 	public void checkIfYearIsDivisibleBy100AndNot400ThanIsNotALeapYear() {
-			LeapYear validator = new LeapYear();
 			String result = validator.checkLeapYear("2100");
 			assertEquals("Leap Year",result);
 	}
