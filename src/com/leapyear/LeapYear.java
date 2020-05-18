aec034069e508430c90d6f23f17a3edb48ad05d1
nilay9 <nil.joshi860@gmail.com>
2020-05-18 14:03:21 +0530

    First test case
    To check 4 digit value

--- src/com/leapyear/LeapYear.java
+++ src/com/leapyear/LeapYear.java
@@ -2,4 +2,13 @@ package com.leapyear;
 
 public class LeapYear {
 
+	public boolean checkLeapYear(String year) {
+		int yearLenght = 4;
+		
+		if ( yearLenght != year.length()) { 
+			return false;
+		}
+		
+		return true;
+	}
 }
--- test/com/testleapyear/TestLeapYear.java
+++ test/com/testleapyear/TestLeapYear.java
@@ -1,14 +1,18 @@
 package com.testleapyear;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import com.leapyear.LeapYear;
 
 class TestLeapYear {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void checkA4DigitYear() {
+		LeapYear validator = new LeapYear();
+		boolean result = validator.checkLeapYear("2000");
+		assertTrue("first value", result);
 	}
 
 }
